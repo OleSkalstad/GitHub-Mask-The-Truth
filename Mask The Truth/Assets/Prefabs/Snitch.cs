07d1378eef0582019ed523e6cc1d963069c667c6
OleSkalstad <72675653+OleSkalstad@users.noreply.github.com>
2026-01-31 18:55:44 +0100

    snitch in progress

--- Mask The Truth/Assets/Prefabs/Snitch.cs
+++ Mask The Truth/Assets/Prefabs/Snitch.cs
@@ -1,8 +1,12 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Snitch : MonoBehaviour
 {
+    [SerializeField] private Transform[] FirstPath;
+
+    private float timer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,11 +16,17 @@ public class Snitch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer+=Time.deltaTime
     }
 
     public void Caught()
     {
         SceneManager.LoadScene("YouGotCaught");
     }
+
+    void GotoFirstTarget()
+    {
+        transform.position=FirstPath[0]*Vector3.Lerp(1-);
+    }
+    
 }
